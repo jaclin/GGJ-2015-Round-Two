af1055c402534e5055b39480b410458cb0a42186
Jac Lin <jaclinyu@gmail.com>
2015-01-31 20:55:23 +0800

    added viewports

--- Pepero.cpp
+++ Pepero.cpp
@@ -1,17 +1,18 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
 #include <stdio.h>
 #include <vector>
 #include <cstdlib>
 #include <cmath>
+#include <string>
 
-const int SCREEN_WIDTH = 1000;
+const int SCREEN_WIDTH = 1300;
 const int SCREEN_HEIGHT = 500;
 
 SDL_Window* gWindow = nullptr;
 SDL_Renderer* renderer = nullptr;
-
-// Class Declaration
+SDL_Texture* pattern = nullptr;
 
 // Class Declaration
 class Player
@@ -127,8 +128,70 @@ bool init()
   return success;
 }
 
+SDL_Texture* loadTexture( std::string path )
+{
+	//The final texture
+	SDL_Texture* newTexture = NULL;
+
+	//Load image at specified path
+	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+	if( loadedSurface == NULL )
+	{
+		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+	}
+	else
+	{
+		//Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface( renderer, loadedSurface );
+		if( newTexture == NULL )
+		{
+			printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+		}
+
+		//Get rid of old loaded surface
+		SDL_FreeSurface( loadedSurface );
+	}
+
+	return newTexture;
+}
+
+bool loadPatternOne()
+{
+	//Loading success flag
+	bool success = true;
+
+	//Load texture
+	pattern = loadTexture( "viewport.png" );
+	if( pattern == NULL )
+	{
+		printf( "Failed to load texture image!\n" );
+		success = false;
+	}
+
+	//Nothing to load
+	return success;
+}
+
+bool loadPatternTwo()
+{
+	//Loading success flag
+	bool success = true;
+
+	//Load texture
+	pattern = loadTexture( "water_PNG3319.png" );
+	if( pattern == NULL )
+	{
+		printf( "Failed to load texture image!\n" );
+		success = false;
+	}
+
+	//Nothing to load
+	return success;
+}
+
 void close()
 {
+    SDL_DestroyTexture(pattern);
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(gWindow);
   gWindow = NULL;
@@ -148,29 +211,57 @@ int main(int argc, char* args[])
   }
   else
   {
-    SDL_Event e;
-    while(!quit)
+    if( !loadPatternOne() )
+		{
+			printf( "Failed to load media!\n" );
+		}
+    else
     {
-      uint capTimer = SDL_GetTicks();
-      while(SDL_PollEvent(&e) != 0)
-      {
-        if(e.type == SDL_QUIT)
+        SDL_Event e;
+        while(!quit)
         {
-          quit = true;
+            uint capTimer = SDL_GetTicks();
+            while(SDL_PollEvent(&e) != 0)
+            {
+                if(e.type == SDL_QUIT)
+                {
+                    quit = true;
+                }
+                if (e.type == SDL_KEYDOWN)
+                    player1->move();
+            }
+
+        // Clear Screen
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        //Pattern viewport
+        SDL_Rect patternViewport;
+        patternViewport.x = 1000;
+        patternViewport.y = 0;
+        patternViewport.w = 300;
+        patternViewport.h = 300;
+        SDL_RenderSetViewport(renderer, &patternViewport);
+
+        SDL_RenderCopy( renderer, pattern, NULL, NULL );
+
+        //Game viewport
+        SDL_Rect mainViewport;
+        mainViewport.x = 0;
+        mainViewport.y = 0;
+        mainViewport.w = 1000;
+        mainViewport.h = 500;
+        SDL_RenderSetViewport(renderer, &mainViewport);
+
+        // Render Objects
+        player1->render();
+
+        // Render other objects
+        SDL_RenderPresent(renderer);
         }
-        if (e.type == SDL_KEYDOWN)
-          player1->move();
-      }
-      // Clear Screen
-      SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-      SDL_RenderClear(renderer);
-      // Render Objects
-      player1->render();
-      // Render other objects
-      SDL_RenderPresent(renderer);
-      //SDL_Delay(200);
     }
   }
+
   close();
   return 0;
 }
